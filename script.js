5f6b474c47ccad4549c6b4dbadce19be55aadad7
simenkrueger <yu_feiyu_fei@hotmail.com>
2026-07-18 22:38:19 +0800

    Update script.js

--- script.js
+++ script.js
@@ -43,6 +43,7 @@ const TAG_CATEGORIES = [
             { key: '铜牌', label: '铜牌' },
             { key: '前五', label: '前五' },
             { key: '前十', label: '前十' }
+            { key: '前30', label: '前30' }
         ]
     },
     {
